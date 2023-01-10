401866c8a40f4507776bcfdbfdeb1ebc47e9caa9
Pikachu <pikachu@nevel.tech>
2023-01-10 17:07:55 +0700

    update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt',
     ],
     css: [
         "bootstrap/dist/css/bootstrap.min.css",

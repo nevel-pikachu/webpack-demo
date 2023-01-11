40e6217331035a7dbbdacd25bb8b8f01c0f136c9
Pikachu <pikachu@nevel.tech>
2023-01-11 09:22:28 +0700

    remove pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-    ],
     css: [
         "bootstrap/dist/css/bootstrap.min.css",
         "@/assets/style/global.scss"
